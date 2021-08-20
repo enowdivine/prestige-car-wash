caa6dfc087797abb9976a8b5d61b2ba317c60569
Sir-Deon <sird655@gmail.com>
2021-08-20 13:35:43 +0100

    setting up the auth

--- src/store/index.js
+++ src/store/index.js
@@ -1,8 +1,9 @@
+import axios from "axios";
 import Vue from "vue";
 import Vuex from "vuex";
 
 Vue.use(Vuex);
-
+axios.defaults.baseURL = "https://car-wash-backend.herokuapp.com/api";
 export default new Vuex.Store({
   state: {},
   mutations: {},
